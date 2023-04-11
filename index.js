de6ab3e17eb1a3c97f2dccce41acc4738ac1c778
Allyson Baffour Owusu <boallyson@gmail.com>
2023-04-10 22:01:10 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,11 +1,63 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+  function scuberGreetingForFeet(feet){
+    if (feet <= 400) {
+      return 'This one is on me!'
+   } else if (feet > 400 && feet <= 2000 ) {
+      return 'That will be twenty bucks.'
+   } else if (feet > 2000 && feet <= 2500) {
+      return 'I will gladly take your thirty bucks.'
+   } else {
+      return 'No can do.'
+   }
+  }
+
+
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." // this is where you declare the initial condition
+        : "No go." // this is the second condition no "if  else or else needed"
+        // This is how we write a ternary statement 
+// function example() {
+//   return condition1 ? value1
+//         : condition2 ? value2
+//         : condition3 ? value3
+//         : value4;
+// }
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function switchOnCharmFromTip(Tip){
+switch (Tip){   
+  case "generous":
+    return "Thank you so much."
+  
+case "not as generous":
+  return "Thank you."
+
+  default : 
+  return "Bye."
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+
+
 }
+
+
+  // this is the syntax for switch statements
+  // switch (expression) {
+  //   case value1:
+  //     statements
+  //   case value2:
+  //     statements
+  //   // …
+  //   case valueN:
+  //     statements
+  //   default:
+  //     statements
+  // }
+  
+  
+
+
+
